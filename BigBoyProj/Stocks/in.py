eee31385a0d5eb6343782623522384db5413392a
JonFactor <Jon.Factor2@gmail.com>
2023-01-06 13:17:26 -0500

    gay

--- BigBoyProj/Stocks/in.py
+++ BigBoyProj/Stocks/in.py
@@ -1,10 +1,14 @@
 import json
+### imports ###
+
 inpit = input('Stock Ticker:')
 inpit = {'ticker':inpit}
 inpit = json.dumps(inpit)
-with open('BigBoyProj\Stocks\localdata.txt', 'w') as op:
+### input ###
+with open('BigBoyProj\Stocks\localdata.json', 'w') as op:
     op.write(inpit)
     op.close()
-
+### make txt file ###
 
 import api
+### start api code ###
