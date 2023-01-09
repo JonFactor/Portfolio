d18df4aec902dad1352ebe192dbc9895efa4a47d
JonFactor <Jon.Factor2@gmail.com>
2023-01-09 16:02:01 -0500

    ffs

---
+++ organizeFiles/moveing.py
@@ -0,0 +1,19 @@
+### imports
+import os, json
+from pathlib import Path
+### functions
+
+def everything():
+    everythingdir = Path.cwd() / 'EVERYTHING'
+    everythingdir.mkdir()
+
+    if '1' in 
+everything()
+def done():
+    if Bools['done']:
+        pass
+### localvars
+with open('organizeFiles/localvars.json', 'r') as loc:
+    Bools = json.load(loc)
+### if statments
+
