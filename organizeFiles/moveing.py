f88cefcc70d2c9124bc6142332716a5c15e4c813
JonFactor <Jon.Factor2@gmail.com>
2023-01-09 16:47:09 -0500

    pleaseBro

--- organizeFiles/moveing.py
+++ organizeFiles/moveing.py
@@ -1,19 +1,27 @@
 ### imports
-import os, json
+import os, json, shutil
 from pathlib import Path
+from datetime import date
+### time
+now = date.today()
 ### functions
-
+def failSafe():
+    if os.path.exists('Backups/AUTO-FAILSAFE'):
+        shutil.rmtree('Backups/AUTO-FAILSAFE')
+    shutil.copytree(os.getcwd(), 'Backups\AUTO-FAILSAFE')
 def everything():
-    everythingdir = Path.cwd() / 'EVERYTHING'
-    everythingdir.mkdir()
-
-    if '1' in 
-everything()
+    if os.path.exists(Path.cwd() / 'EVERYTHING'):
+        pass
+    else:
+        everythingdir = Path.cwd() / 'EVERYTHING'
+        everythingdir.mkdir()
 def done():
     if Bools['done']:
         pass
+
 ### localvars
 with open('organizeFiles/localvars.json', 'r') as loc:
     Bools = json.load(loc)
-### if statments
-
+    
+### function order
+failSafe()
