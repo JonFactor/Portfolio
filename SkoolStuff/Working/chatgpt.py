8a2f1fcfe3b8868f4dc07e99a0de8e7a1b8f2232
JonFactor <Jon.Factor2@gmail.com>
2023-01-09 18:01:11 -0500

    finnally

--- SkoolStuff/Working/chatgpt.py
+++ SkoolStuff/Working/chatgpt.py
@@ -1,56 +1,46 @@
-# Import libraries
+
 from bs4 import BeautifulSoup
 import requests
 import csv
  
-# Set the URL to scrape
+
 url = "https://weather.com/weather/tenday/l/USCA0987:1:US"
  
-# Send a GET request to the URL and store the response
+
 response = requests.get(url)
  
-# Parse the response using BeautifulSoup
+
 soup = BeautifulSoup(response.text, "html.parser")
+
+
+forecast_container = soup.find(class_="DailyForecast--DisclosureList--nosQS")
  
-# Find the forecast container element
-forecast_container = soup.find(class_="twc-table")
- 
-# Create an empty list to store the forecast data
+dayData = []
 forecast_data = []
- 
-# Loop through each row in the forecast table
-for row in forecast_container.find_all("tr"):
-  # Create an empty dictionary to store the forecast data for each day
-  day_data = {}
- 
-  # Loop through each cell in the row
-  for cell in row.find_all("td"):
-    # Get the forecast date and store it in the dictionary
-    if cell.get("headers")[0] == "date":
-      day_data["date"] = cell.text
- 
-    # Get the forecast description and store it in the dictionary
-    if cell.get("headers")[0] == "description":
-      day_data["description"] = cell.text
- 
-    # Get the forecast high and low temperatures and store them in the dictionary
-    if cell.get("headers")[0] == "hi-lo":
-      temperatures = cell.text.split("/")
-      day_data["high"] = temperatures[0]
-      day_data["low"] = temperatures[1]
- 
-  # Add the day's forecast data to the list
-  forecast_data.append(day_data)
- 
-# Open a new file to write the forecast data to
+allDays = forecast_container.find_all(class_='DetailsSummary--daypartName--kbngc')
+allDays = str(allDays).replace('<h3 class="DetailsSummary--daypartName--kbngc" data-testid="daypartName">' , '').replace('</h3>', '').split(',')
+
+allTemps = forecast_container.find_all(class_='DetailsSummary--temperature--1kVVp')
+
+allTemps = str(allTemps).replace('[<div class="DetailsSummary--temperature--1kVVp" data-testid="detailsTemperature"><span class="DetailsSummary--highTempValue--3PjlX" data-testid="TemperatureValue">59°</span><span data-testid="lowTempValue">/<span class="DetailsSummary--lowTempValue--2tesQ" data-testid="TemperatureValue">' , '')
+allTemps = allTemps.replace(' <div class="DetailsSummary--temperature--1kVVp" data-testid="detailsTemperature"><span class="DetailsSummary--highTempValue--3PjlX" data-testid="TemperatureValue">', '')
+allTemps = allTemps.replace('</span></span></div>,', '').replace('</span><span data-testid="lowTempValue">/<span class="DetailsSummary--lowTempValue--2tesQ" data-testid="TemperatureValue">', '')
+allTemps = allTemps.replace('</span></span></div>]', '')
+allTemps = allTemps.split('°')
+
+print(len(allTemps))
+x = 1
+i = 0
 with open("forecast.csv", "w") as csv_file:
-  # Create a CSV writer
-  writer = csv.DictWriter(csv_file, fieldnames=["date", "description", "high", "low"])
- 
-  # Write the column headers
-  writer.writeheader()
- 
-  # Write the forecast data to the file
-  for day in forecast_data:
-    writer.writerow(day)
- 
+  while i <= 5:
+    if x < len(allTemps):
+      math =(int(allTemps[i]) + int(allTemps[x])) / 2
+      dayData.append(allDays[i])
+      dayData.append(math)
+    else:
+      pass
+
+    csv_file.writelines(f'Day:{dayData[i]} Temp:{dayData[x]} | ')
+    x +=2
+    i +=1
+############## i hate these comments ;-;
