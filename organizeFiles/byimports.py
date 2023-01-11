d3c0566cc0dd3af96a7a08eee459532e96aa47ea
JonFactor <Jon.Factor2@gmail.com>
2023-01-11 03:26:12 -0500

    maybe?

--- organizeFiles/byimports.py
+++ organizeFiles/byimports.py
@@ -2,35 +2,55 @@
 from pathlib import Path
 import pathlib, os, shutil,json
 ### first things first
-os.remove('organizeFiles\\byimportsTemp.json')
+if os.path.exists('PRODUCT\BY-IMPORTS'):
+    shutil.rmtree('PRODUCT\BY-IMPORTS')
+
+
+baseDir = Path.cwd() / 'PRODUCT' / 'BY-IMPORTS'
+baseDir.mkdir(exist_ok=True,parents=True)
 ### grabbing
 with open('organizeFiles/localvars.json', 'r') as loc:
     Bools = json.load(loc)
 ### functions
-def ifdone():
-    global baseDir
-    if Bools['Done']:
-        baseDir = Path.cwd() / 'PRODUCT' /'WORKING'
-    else:
-        baseDir = Path.cwd() / 'PRODUCT'
-def writing():
-    for dir in os.listdir('RAW'):
-        for file in os.listdir(f'Raw\\{dir}'):
-            if '.py' in file:
-                with open(f'Raw\\{dir}\\{file}') as first, open('organizeFiles\\byimportsTemp.json', 'a') as second:
-                    for line in first:
-                        if not '#' in line:
-                            if 'from' in line or 'import' in line:
-                                tempdict = {
-                                    'import' :line.replace('\n', ''),
-                                    'file':file
-                                }
-                                second.write(json.dumps(tempdict))
-                                second.write('\n')
-def placeing():
-    
 
+def working():
+    filelist = []
+    for dir in os.listdir('RAW'):
+        rawDir = f'Raw\\{dir}'
+        for file in os.listdir(rawDir):
+            if '.py' in file:
+                filelist.append(file)
+
+                for line in open(f'Raw\\{dir}\\{file}'):
+                    if not '#' in line:
+                        if 'import' in line:
+
+                            line2 = line.replace('\n', '').replace(' ','-')
+                            filedir = baseDir / line2 
+                            if not os.path.exists(filedir):
+                                filedir.mkdir(exist_ok=True,parents=True)
+
+                        else:
+                            noimportdir = baseDir / 'NO IMPORTS'
+                            if not os.path.exists(baseDir / 'NO IMPORTS'):
+                                noimportdir.mkdir(exist_ok=True,parents=True)
+                if line in file:
+                    filelist.append(dir)
+                for filee in filelist:
+                    if not os.path.exists(filedir/dir):
+                        shutil.copytree(rawDir, filedir/dir)
+
+
+def refine():
+    for dir in os.listdir('PRODUCT\BY-IMPORTS'):
+        Dir = f'PRODUCT\BY-IMPORTS\\{dir}'
+        isempty = os.stat(Dir)
+        isempty = isempty.st_size
+        
+        if os.path.exists(Dir):
+            if isempty == 0:
+                os.rmdir(Dir)
 ###functionOrder
-ifdone()
-writing()
+working()
+refine()
 ### finishing
